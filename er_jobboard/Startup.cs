60e01558affdfd98158110ce26a7a1bf6341e1be
Edwin Ramirez <erpramirez@outlook.com>
2020-10-26 23:43:03 -0600

    Add project files.

---
+++ er_jobboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(er_jobboard.Startup))]
+namespace er_jobboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
